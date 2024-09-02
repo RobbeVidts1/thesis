c9da39d17ffc93a8761afc9092a73793cec35693
Robbe_home <vidts.robbe@gmail.com>
2024-09-02 10:50:37 +0200

    2/09/2024,plotted the unbounded heatcapacity

--- paper/macro_plotter.py
+++ paper/macro_plotter.py
@@ -2,7 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from basic_units import radians
 
-plt.rcParams['text.usetex'] = True
+from paper.macro_solver import HeatCap_unbounded
+
+# plt.rcParams['text.usetex'] = True
 
 
 def first_hyst_fig_plot():
@@ -158,8 +160,8 @@ def infl_omega_0_plot():
 
     ax_alt_cold.plot(stat_data[0], stat_data[1], label="stationary solution")
 
-    ax_alt_cold.set_xlabel("$h(t)$")
-    ax_alt_cold.set_ylabel("$m(t)$")
+    ax_alt_cold.set_xlabel("$h_t$")
+    ax_alt_cold.set_ylabel("$m_t$")
 
     plt.grid(True)
     ax_alt_cold.legend()
@@ -168,9 +170,55 @@ def infl_omega_0_plot():
     plt.show()
 
 
+def unbounded_heatcap_plot():
+    # physical parameters
+    omega_0 = 0.02
+    omega_beta=0.001
+
+    # load the data
+    data = np.load("Heatcap_unbounded.npy")
+
+    # plotparameters
+    dpi_set = 100.0
+    figsize_set = [6.4,4.8]
+    plt.rcParams.update({
+        "axes.labelsize": 15,    #size of labels
+        "grid.alpha": .5,       #visibility of grid
+        "grid.color": "grey",
+        "grid.linestyle": "-",
+        "grid.linewidth": .4,
+        "legend.fontsize": 15,
+        "legend.loc": "upper left",
+        "lines.linewidth": 1.5,
+        "xtick.labelsize": 15,  #size of ticks
+        "ytick.labelsize": 15
+    })
+
+    # plotting
+    fig, ax = plt.subplots(figsize=figsize_set, dpi=dpi_set, layout='constrained')
+
+    plt.plot(data[0], data[1], label=r"$h_0 = 0.01$")
+    plt.plot(data[0], data[2], label=r"$h_0 = 0.08$")
+    plt.plot(data[0], data[3], label=r"$h_0 = 0.16$")
+    plt.plot(data[0], data[4], label=r"$h_0 = 0.3$")
+
+    ax.set_xlabel(r"$\beta_0$")
+    ax.set_ylabel("$C$")
+
+    ax.set_xlim(0,2.6)
+    ax.set_ylim(-2,2.2)
+
+    plt.grid(True)
+    ax.legend()
+
+    plt.show()
+
+
+
 def main():
     # first_hyst_fig_plot()
-    infl_omega_0_plot()
+    # infl_omega_0_plot()
+    unbounded_heatcap_plot()
 
 if __name__ == '__main__':
     main()
